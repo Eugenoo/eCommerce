0d593b4ac3968385aabeed7fca51fb8f03a988f3
Filip Tuliszkiewicz <filiptuliszkiewicz@Filips-MacBook-Pro.local>
2023-11-13 16:28:55 +0100

    admin blade changes

--- Back/resources/views/admin.blade.php
+++ Back/resources/views/admin.blade.php
@@ -16,7 +16,6 @@
         @vite('resources/css/app.css')
     </head>
     <body>
-
         <button data-drawer-target="default-sidebar" data-drawer-toggle="default-sidebar" aria-controls="default-sidebar" type="button" class="inline-flex items-center p-2 mt-2 ml-3 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
            <span class="sr-only">Open sidebar</span>
            <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
@@ -75,7 +74,7 @@
                        <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 16">
                           <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 8h11m0 0L8 4m4 4-4 4m4-11h3a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2h-3"/>
                        </svg>
-                       <span class="flex-1 ml-3 whitespace-nowrap">Sign In</span>
+                       <span class="flex-1 ml-3 whitespace-nowrap">Sign Out</span>
                     </a>
                  </li>
                  <li>
